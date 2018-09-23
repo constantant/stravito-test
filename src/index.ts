7a4b0c5ae232530e9a693d73a78bcd1d4fd0245d
Konstantin Kharitonov <kharitonov.k.y@gmail.com>
2018-09-23 21:38:21 +0200

    avatar

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import { App } from './app';
 const app = new App({
     firstName: 'Konstantin',
     lastName: 'Kharitonov',
-    photo: 'https://media.licdn.com/dms/image/C5603AQEPTQPbJ3V5qg/profile-displayphoto-shrink_100_100/0?e=1532563200&v=beta&t=WvDNtwVJqRKZr9GP3PNMEDY6crdMyJpu9Vgz-4OspEI'
+    photo: 'https://avatars1.githubusercontent.com/u/5537730?v=4'
 });
 app.run();
